66b78610136f6c5bf948d381dfc5f779ca6165d5
Lukas22041 <lukas.kurth04@gmail.com>
2025-04-08 01:32:15 +0200

    Added WItchcraft Codex entry

--- jars/src/data/scripts/campaign/secondInCommand/AptitudeWitchcraft.kt
+++ jars/src/data/scripts/campaign/secondInCommand/AptitudeWitchcraft.kt
@@ -1,11 +1,21 @@
 package data.scripts.campaign.secondInCommand
 
 import com.fs.starfarer.api.campaign.CampaignFleetAPI
+import com.fs.starfarer.api.ui.TooltipMakerAPI
+import com.fs.starfarer.api.util.Misc
 import second_in_command.SCData
 import second_in_command.specs.SCAptitudeSection
 import second_in_command.specs.SCBaseAptitudePlugin
 
 class AptitudeWitchcraft : SCBaseAptitudePlugin() {
+
+    override fun addCodexDescription(tooltip: TooltipMakerAPI) {
+        tooltip.addPara("The Witchcraft aptitude is added by the Secrets of the Frontiers mod. " +
+                "The aptitude provides a variety of benefits, some for sierra herself, some for phase ships and others with a bit of a magical touch. " +
+                "It is acquired by completing the wayward star quest, interacting with both the vow and wispmother wreck, and then starting a dialog with sierra through the contact in the intel screen. ",
+            0f, Misc.getTextColor(), Misc.getHighlightColor(), "Witchcraft", "acquired")
+    }
+
     override fun getOriginSkillId(): String {
         return "sotf_dance_between_realms"
     }
